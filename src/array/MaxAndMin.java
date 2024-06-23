259256773800aee5d91cad5de76cda1d111d11c1
Sourabh Mishra <saurabhm0037@gmail.com>
2024-06-23 18:32:11 +0530

    First Program Added

--- src/array/MaxAndMin.java
+++ src/array/MaxAndMin.java
@@ -1,10 +1,28 @@
 package array;
 
+import java.util.Scanner;
+
 public class MaxAndMin {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+	    Scanner s = new Scanner(System.in);
+	    int n=s.nextInt();
+	    int[] arr = new int[n];
+	    for(int i=0;i<arr.length;i++)
+	    {
+	    	arr[i]=s.nextInt();
+	    }
+	    
+	    int max=Integer.MIN_VALUE;
+	    int min=Integer.MAX_VALUE;
+	    for(int i=0;i<arr.length;i++)
+	    {
+	    	max=Math.max(max,arr[i]);
+	    	min=Math.min(min,arr[i]);
+	    }
+	    System.out.println("Max value of array is " + max + " Min value of array is " + min);
+	    s.close();
 	}
 
 }
